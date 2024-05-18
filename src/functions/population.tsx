92eef8f95f1b2d048a37e5f1aa626f960aa6acfd
Parastoo-Imeny <parastoo.imeny@gmail.com>
2024-05-18 11:06:46 +0330

    population function.

--- src/functions/population.tsx
+++ src/functions/population.tsx
@@ -1,12 +1,12 @@
 type Population = number;
 
 const population = (population: Population) => {
-const result = population.toString().length >= 10
-? `${(population / 1000000000).toFixed(1)} billion`
-: population.toString().length >= 7
-? `${(population / 1000000).toFixed(1)} million`
-: population.toString().length >= 4
-? `${(population / 1000).toFixed(1)} thousand`
+const result = population?.toString().length >= 10
+? `${(population / 1000000000)?.toFixed(1)} billion`
+: population?.toString().length >= 7
+? `${(population / 1000000)?.toFixed(1)} million`
+: population?.toString().length >= 4
+? `${(population / 1000)?.toFixed(1)} thousand`
 : `${population}`;
 
   return result;
