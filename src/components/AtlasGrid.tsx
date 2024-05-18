428a67415869577c2f9d384ac74eb78873f869c2
Parastoo-Imeny <parastoo.imeny@gmail.com>
2024-05-18 10:50:18 +0330

    Create AtlasGrid component.

---
+++ src/components/AtlasGrid.tsx
@@ -0,0 +1,27 @@
+import Country from "../interfaces/Country";
+import Card from "./Card";
+import useData from "./hooks/useData";
+
+interface Props {
+  endPoint: string;
+}
+
+function AtlasGrid({ endPoint }: Props) {
+  const { data, error, isLoading } = useData<Country[]>(endPoint);
+
+  console.log("endPoint", endPoint)
+  console.log("data", data)
+
+  if (isLoading) return "Is Loading.....";
+  if (error) return <p className="text-red-500">{error}</p>;
+
+  return (
+    <section className="grid gap-[30px] md:grid-cols-3 lg:grid-cols-4">
+      {data?.map((country, index) => {
+        return <Card key={index} country={country} id={index} />;
+      })}
+    </section>
+  );
+}
+
+export default AtlasGrid;
