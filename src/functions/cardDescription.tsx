3dcfadece49c56452c7651f76a57fc371eb96e79
Parastoo-Imeny <parastoo.imeny@gmail.com>
2024-05-18 11:20:13 +0330

    Add a dynamic description to card component

--- src/functions/cardDescription.tsx
+++ src/functions/cardDescription.tsx
@@ -3,12 +3,12 @@ import population from "./population";
 
 const cardDescription = (country: Country) => {
   return (
-    <p className="font-normal opacity-70">{`${country.name.official} is ${
-      country.independent ? "an indepebdent" : "a dependent"
+    <p className="font-normal opacity-70">{`${country?.name?.official} is ${
+      country?.independent ? "an indepebdent" : "a dependent"
     } country in ${
-      country.subregion
+      country?.subregion
     } with an estimated population of over ${population(
-      country.population
+      country?.population
     )} people.`}</p>
   );
 };
