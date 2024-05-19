4ca6cbcce3b38e8fc5eac5e20a14a972dc9bdca8
Parastoo-Imeny <parastoo.imeny@gmail.com>
2024-05-19 12:50:39 +0330

    Sort Country interface.

--- src/interfaces/Country.ts
+++ src/interfaces/Country.ts
@@ -1,28 +1,29 @@
 export default interface Country {
-  independent: boolean;
-  subregion: string;
-  population: number;
-  capital: string[];
   area: number;
-  startOfWeek: string;
-  timezones: string[];
   borders: string[];
-  languages: { [key: string]: string };
-  currencies: {[key: string]: { [key: string]: string }};
-  demonyms: {[key: string]: { f : any }};
-  car: { [key: string] : string };
-  gini: {[key: number]: number};
-  idd:{root: string}
-  maps: {googleMaps: string,
-    openStreetMaps: string
-   };
+  capital: string[];
+  car: { side: string; signs: string[] };
+  ccn3: string;
+  currencies: { [key: string]: { name: string; symbol: string } };
+  demonyms: { [key: string]: { f: any } };
   flags: {
     svg: string;
     png: string;
     alt: string;
   };
+  gini: { [key: number]: number };
+  idd: { root: string; suffixes: string[] };
+  independent: boolean;
+  landlocked: boolean;
+  languages: { [key: string]: string };
+  maps: { googleMaps: string; openStreetMaps: string };
   name: {
     common: string;
     official: string;
   };
+  population: number;
+  region: string;
+  startOfWeek: string;
+  subregion: string;
+  timezones: string[];
 }
