334008b75fc115bc417f2574be039dc523c5f4af
Parastoo-Imeny <parastoo.imeny@gmail.com>
2024-05-19 15:40:38 +0330

    Create HorizontalScroll component.

---
+++ src/components/HorizontalScroll.tsx
@@ -0,0 +1,20 @@
+// import Card from "./Card";
+
+// function HorizontalScroll() {
+
+//     return (
+//       <section className="relative overflow-hidden">
+//           {countries.map((country, index) => {
+//               return(
+//                   <div className="transition-transform relative">
+//                       <Card key={index} country={country} id={index} />
+//                   </div>
+//               )
+//           })}
+//       </section>
+//     );
+// }
+
+// export default HorizontalScroll;
+
+// // className="overflow-scroll flex whitespace-nowrap duration-0  w-auto translate-x-0	translate-y-0 translate-z-0"
