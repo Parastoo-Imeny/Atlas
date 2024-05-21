1f8520000b3c34fc446df7811a5a219979a3ac66
Parastoo-Imeny <parastoo.imeny@gmail.com>
2024-05-21 11:20:32 +0330

    Register new styles for Tailwind with Plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
     fontFamily: {
-      'Inter': ['Inter', 'serif'] // Ensure fonts with spaces have " " surrounding it.
+      Inter: ["Inter", "serif"], // Ensure fonts with spaces have " " surrounding it.
     },
   },
-  plugins: [],
-}
-
+  plugins: [
+    plugin(function ({ addUtilities, addComponents, e, config }) {
+      // Add your custom styles here
+      addUtilities({
+        "&::-webkit-scrollbar": {
+          display: "none",
+        },
+        ".scrollbar-hide": {
+          "-ms-overflow-style": "none",
+          "scrollbar-width": "none",
+        },
+        ".overflow-scrollling": {
+          "overflow-scrolling": "touch",
+        },
+      });
+    }),
+  ],
+};
